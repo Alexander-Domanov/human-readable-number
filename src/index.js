c33bcdc4ab343cb192bda5d325846d1f5951c980
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2025-08-10 16:20:25 +0300

    feat: add algoritm readable nubmer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(n) {
+  const ones = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  const teens = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  if (n < 10) return ones[n];
+  if (n >= 10 && n < 20) return teens[n - 10];
+  if (n < 100) {
+    const firstN = Math.floor(n / 10);
+    const secondN = n % 10;
+    if (secondN === 0) return tens[firstN];
+    return `${tens[firstN]} ${ones[secondN]}`;
+  }
+  if (n < 1000) {
+    const firstN = Math.floor(n / 100);
+    const secondN = n % 100;
+    return `${ones[firstN]} hundred${secondN ? ` ${toReadable(secondN)}` : ''}`;
+  }
+
+  return n;
 };
